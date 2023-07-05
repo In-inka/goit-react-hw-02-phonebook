abe96866c46b8229e7e6b07a98778b7844709e05
Inna Shkilna <shkinai.1995@gmail.com>
2023-07-05 17:58:48 +0300

    edit ContactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,14 +1,14 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import { List, Item, Btn, Contact } from './ContactList.styled';
 
 const ContactList = ({ contacts, filter, onDeleteContact }) => {
-  const contactItem = filter
-    ? contacts.filter(({ name }) => name.toLowerCase().includes(filter))
-    : contacts;
+  const contactItem = () =>
+    filter
+      ? contacts.filter(({ name }) => name.toLowerCase().includes(filter))
+      : contacts;
   return (
     <List>
-      {contactItem.map(({ id, name, number }) => (
+      {contactItem().map(({ id, name, number }) => (
         <Item key={id}>
           <Contact> {`${name}: ${number}`}</Contact>
           <Btn type="button" onClick={() => onDeleteContact(id)}>
@@ -21,7 +21,13 @@ const ContactList = ({ contacts, filter, onDeleteContact }) => {
 };
 
 ContactList.propTypes = {
-  contacts: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired,
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ).isRequired,
   filter: PropTypes.string,
   onDeleteContact: PropTypes.func,
 };
